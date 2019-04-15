e84b565223c5a7b91c3c468eed81a08d0a113245
Rene Treffer <treffer@measite.de>
2019-04-15 21:23:23 +0200

    Switch back to packaged upstream openwrt node_exporter

--- openwrt-all/overlay/usr/lib/lua/prometheus-collectors/conntrack.lua
+++
@@ -1,8 +0,0 @@
-local function scrape()
-  metric("node_nf_conntrack_entries", "gauge", nil,
-    string.sub(get_contents("/proc/sys/net/netfilter/nf_conntrack_count"), 1, -2))
-  metric("node_nf_conntrack_entries_limit", "gauge", nil,
-    string.sub(get_contents("/proc/sys/net/netfilter/nf_conntrack_max"), 1, -2))
-end
-
-return { scrape = scrape }
--- openwrt-all/overlay/usr/lib/lua/prometheus-collectors/time.lua
+++
@@ -1,6 +0,0 @@
-local function scrape()
-  -- current time
-  metric("node_time_seconds", "counter", nil, os.time())
-end
-
-return { scrape = scrape }
